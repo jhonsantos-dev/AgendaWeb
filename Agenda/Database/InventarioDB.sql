5f80e3f00b25d54a4b63b7a617317a4f9d3a8a28
Jhon Fredy Santos Niño <fredysan99@gmail.com>
2026-05-04 19:55:58 -0500

    Agregando script de base de datos

---
+++ Agenda/Database/InventarioDB.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE AgendaContactos
+go
+USE AgendaContactos
+go
+Create table Contactos(
+Id_Contacto int primary key identity(1,1),
+Nombre nVarchar(100) not null,
+Apellido nvarchar (100) not null,
+Telefono nvarchar(20) not null,
+Email nvarchar(100) not null,
+Direccion nvarchar(100) not null
+);
